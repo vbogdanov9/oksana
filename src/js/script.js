243a0a90c218a32f876f9249869069aa5f581000
Seva <vbogdanov9@gmail.com>
2024-02-22 01:17:13 +0300

    swiper step 2

--- src/js/script.js
+++ src/js/script.js
@@ -5,18 +5,20 @@ const swiper = new Swiper('.swiper', {
     grabCursor: true,
   
     // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
   
     // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
   
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
+      // draggable: true,
     },
   });
