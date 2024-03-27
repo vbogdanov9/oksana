7d1d6f7b8a5953b1f78bcd0d0232ac9db9f4c249
Seva <vbogdanov9@gmail.com>
2024-03-28 01:15:39 +0300

    adapt cases section on 1280px

--- src/js/script.js
+++ src/js/script.js
@@ -33,11 +33,20 @@ const swiper = new Swiper('.swiper', {
   const cases_swiper = new Swiper('.cases__swiper', {
     slidesPerView: 3,
     speed: 400,
-    spaceBetween: 85,
     navigation: {
       nextEl: ".cases__button-next",
       prevEl: ".cases__button-prev",
     },
+    breakpoints: {
+      // when window width is >= 1441px
+      1441: {
+        spaceBetween: 85
+      },
+      // when window width is >= 1280px
+      1280: {
+        spaceBetween: 32
+      },
+    }
   });
 
 
